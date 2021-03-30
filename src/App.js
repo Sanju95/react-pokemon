e6e770805753db702122a46e159a79e7a3ac0f39
unknown <k.sanjeevan@gmail.com>
2021-03-30 02:06:45 +0100

    Pokemon list created using images

--- src/App.js
+++ src/App.js
@@ -2,17 +2,48 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import PokemonList from "./Pokemon-1/PokemonList";
+import Card from "./Pokemon-1/Card";
 
 function App() {
+  const endPoint = "https://pokeapi.co/api/v2/pokemon?limit=151";
   const [pokemon, setPokemon] = useState([]);
+  const [pokemonData, setPokemonData] = useState([]);
+  //const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151").then((res) => {
-      setPokemon(res.data.results.map((p) => p.name));
-    });
+    getGen1Pokemons();
   }, []);
 
-  return <PokemonList pokemon={pokemon} />;
+  const getGen1Pokemons = async () => {
+    let res = await axios.get(endPoint);
+    let data = await res.data;
+    setPokemon(data.results.map((p) => p.name));
+    await getPokemonData(data.results);
+    //console.log(res);
+  };
+
+  const getPokemonData = async (pData) => {
+    let res = await Promise.all(
+      pData.map(async (p) => {
+        let data = await axios.get(p.url);
+        return data;
+      }),
+    );
+    let dataP = res.map((p) => p.data);
+
+    console.log(dataP);
+
+    setPokemonData(dataP);
+  };
+
+  return (
+    <div>
+      {pokemonData.map((p, i) => {
+        return <Card key={i} pokemon={p} />;
+      })}
+      <PokemonList pokemon={pokemon} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pokemon-1/Card.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Card = ({ pokemon }) => {
+  return (
+    <div>
+      <ul>
+        <li>{pokemon.name}</li>
+        <li>{pokemon.weight}</li>
+        <li>{pokemon.height}</li>
+        <li>
+          {pokemon.abilities.map((abi, i) => {
+            return (
+              <ul key={i}>
+                <li>{abi.ability.name}</li>
+              </ul>
+            );
+          })}
+        </li>
+        <li>
+          <img
+            src={pokemon.sprites.other.dream_world.front_default}
+            alt={pokemon.name}
+          />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Card;
